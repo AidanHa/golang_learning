6a7b45b530e5e6431387c6fa4ce399743e7b326f
AIDAN HA <qbha@edu.uwaterloo.ca>
2021-01-28 16:09:56 -0500

    syntax for variables and printing
    Signed-off-by: AIDAN HA <qbha@edu.uwaterloo.ca>

--- main.go
+++ main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
-    // var number uint16 = 260
-	fmt.Println("Hello World")
+	// Explicit
+	/*
+	var number uint16 = 260
+	number = number + 10
+
+	var number2 = 10
+	var string = "10"
+
+	// Easiest way to declare a variable
+	number3 := "6"
+	// Can't do anything like "number = 5 bc variable type is string"
+	number3 = "changed"
+
+	fmt.Println("Hello World", number)
+	fmt.Println("Goodbye world" + string)
+	fmt.Printf("%T\n", number2)
+	fmt.Printf("%T\n", number3)
+	fmt.Println("Test", number3)
+	*/
+	var b1 bool
+	fmt.Println(b1)
 }
